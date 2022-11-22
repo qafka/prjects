8aa3ab9fe6170c028ab99a51e2804968a3a32910
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2022-11-22 10:15:40 -0500

    Update dataRead.py

--- dataRead.py
+++ dataRead.py
@@ -15,14 +15,27 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict]) #what we're trying to predict - label
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+'''
+best = 0
+for _ in range(20):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train) #finding a best fit line
-acc = linear.score(x_test, y_test)
-print(acc)
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print("Accuracy: " + str(acc))
+    
+    # If the current model has a better score than one we've already trained then save it
+    if acc > best:
+        best = acc
+        with open("studentModel.pickle", "wb") as f:
+            pickle.dump(linear, f)'''
+
+pickle_in = open("studentModel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 print('Co: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
@@ -32,3 +45,9 @@ predictions = linear.predict(x_test)
 for x in range(len(predictions)):
   print(predictions[x], x_test[x], y_test[x])
 
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
